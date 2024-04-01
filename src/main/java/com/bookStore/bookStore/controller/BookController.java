ca8fe30d4b73052874c22f64a771a566d95def55
MAMATHA-PADAVALA <mamathapadavala146@gmail.com>
2024-04-01 10:33:02 +0530

    Add. API to fetch list of books

---
+++ src/main/java/com/bookStore/bookStore/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.bookStore.bookStore.controller;
+
+import com.bookStore.bookStore.dto.BookDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @GetMapping(path = "/books")
+    public ResponseEntity<List<BookDto>> getBooks(){
+        BookDto book= BookDto.builder()
+                .title("My First book title")
+                .build();
+        List<BookDto> books=new ArrayList<>();
+        books.add(book);
+        return ResponseEntity.ok(books);
+    }
+}
---
+++ src/main/java/com/bookStore/bookStore/dto/BookDto.java
@@ -0,0 +1,14 @@
+package com.bookStore.bookStore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BookDto {
+    private String title;
+}
